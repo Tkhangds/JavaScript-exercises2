8f146f3869583169e13f92a5b2991ec2bca7ba6b
Tkhangds <87261264+Tkhangds@users.noreply.github.com>
2024-03-07 10:12:35 +0700

    submit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(sum, num) {
+    if(num < 0){
+        return "ERROR";
+    }
+    let ans = "";
+    for(let i = 0; i < num;i++){
+        ans += sum;
+    }
+    return ans;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let ans = 0,start = 0,end = 0;
 
+    if(typeof(num1) != typeof(1) || typeof(num2) != typeof(1)){
+        return "ERROR";
+    }
+
+    if(num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    
+    if(num1 > num2){
+        start = num2;
+        end = num1;
+    } else {
+        start = num1;
+        end = num2;
+    }
+
+    for(let i = start; i < end + 1;i++){
+        ans += i;
+    }
+    return ans;
 };
 
 // Do not edit below this line
